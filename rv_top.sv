d39d578ceaaf1932602c52a59989b778353ee9f0
Nguyen Thanh Dat <123445659+danielvinC@users.noreply.github.com>
2024-05-28 13:49:31 +0700

    Update rv_top.sv

--- rv_top.sv
+++ rv_top.sv
@@ -1,17 +1,10 @@
-module rv_top( input logic clk, reset,
-            output logic [31:0] WriteDataM, DataAdrM,
-            output logic MemWriteM,PCstall,
-
-    output logic [31:0] PCF, InstrF, ReadDataM
-	 ,output logic [31:0] dec
-	 );
+module rv_top( 	input logic clk, reset,
+	        output logic [31:0] WriteDataM, DataAdrM,
+	        output logic MemWriteM,PCstall,
+		output logic [31:0] PCF, InstrF, ReadDataM, dec);
 
     // instantiate processor and memories
-    riscv riscv(clk, reset, PCF, InstrF, MemWriteM
-	 , PCstall
-	 , DataAdrM, WriteDataM, ReadDataM
-	 , dec
-	 );
+    riscv riscv(clk, reset, PCF, InstrF, MemWriteM, PCstall, DataAdrM, WriteDataM, ReadDataM, dec);
     imem imem(PCF, InstrF);
     dmem dmem(clk, MemWriteM, DataAdrM, WriteDataM, ReadDataM);
 endmodule
