e23c61032f6e67351f0750ced0f091ebf7c7481c
Nguyen Thanh Dat <123445659+danielvinC@users.noreply.github.com>
2024-12-06 19:02:29 +0700

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -3,32 +3,41 @@ module testbench();
     logic reset;
     logic [31:0] WriteData, DataAdr;
     logic MemWrite, PCstall;
-    logic [31:0] PCF, InstrF, ReadDataM, dec;
+	 logic [31:0] PCF, InstrF, ReadDataM
+	 ,dec
+	 ; logic mispredict, PCSrcE, sourceF
+     ; logic [31:0] PCTargetE
+	 ; logic [17:0] t
+     ; logic ins_vld
+     ;
 // instantiate device to be tested
-	rv_top dut(clk, reset, WriteData, DataAdr, MemWrite,PCstall, PCF, InstrF, ReadDataM, dec);
+    rv_top dut(clk, reset, WriteData, DataAdr, MemWrite,PCstall, PCF, InstrF, ReadDataM
+	 ,dec
+     ,mispredict, sourceF, PCSrcE
+     , PCTargetE
+     , t
+     , ins_vld
+	 );
 // initialize test
     initial
         begin
-            reset <= 1; # 22; reset <= 0;
+            reset <= 0; # 22; reset <= 1;
         end
     // generate clock to sequence tests
     always
         begin
             clk <= 1; # 5; clk <= 0; # 5;
         end
-//   check results
+
+    //   check results
     always @(negedge clk)
     begin
         if(MemWrite) begin
-        if(DataAdr === 96 & WriteData === 3) begin
-            $display("Simulation succeeded");
-				#5
-            $stop;
-        end else if (DataAdr !== 96) begin
-            $display("Simulation failed");
-				#5
-            $stop;
-        end
+            if(DataAdr === 96 & WriteData === 3) begin
+                $display("Simulation succeeded");
+                #10
+                $stop;
+            end 
         end
     end
 endmodule
